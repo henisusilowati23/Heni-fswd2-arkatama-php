1bdf29ca7edb8957ac964cfefca91d15b35bdc3e
henisusilowati23 <henisusilowati2301@gmail.com>
2023-04-15 18:33:41 +0700

    first commit

---
+++ PHP/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my biodata</title>
+</head>
+<body>
+<header>
+    <h1 align="center">MY BIODATA</H1>
+</header>
+    <?php
+    echo 'Nama : Heni Susilowati<br>';
+    echo 'TTL : Jember, 23 Juni 2001<br>';
+    echo 'Jenis Kelamin : Perempuan<br>';
+    echo 'Alamat Lengkap : Jl. Watu Ulo RT 003 Rw 035 Sumberrejo Ambulu Jember<br>'; 
+    echo 'Activities / Hobby : Travelling, Badminton, Mendengarkan Musik, Menyanyi, Membaca Buku<br>';
+    echo 'Education :';
+    echo '2017-2020 - SMK Ash- Shiddiqi Curahlele Balung/ Jurusan Teknik Komputer & Jaringan<br>';
+    echo '2020 - Sekarang - Universitas Merdeka Malang / Jurusan Sistem Informasi<br>';
+    
+
+    phpinfo();
+    ?>
+</body>
+</html>
